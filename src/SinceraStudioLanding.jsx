de4cf1f23a222f146efaeb3e9104d462b8b92441
BITGADGET <bitgadgetshop@gmail.com>
2025-06-16 20:31:04 +0000

    Modifiche al banner e al quick menu mobile

--- src/SinceraStudioLanding.jsx
+++ src/SinceraStudioLanding.jsx
@@ -215,7 +215,7 @@ export default function LastraCeramicaLanding() {
         <div
           className="absolute inset-0 z-0 bg-cover bg-center"
           style={{
-            backgroundImage: "url('/src/assets/banner.webp')",
+            backgroundImage: "url('/public/banner.webp')",
             filter: "brightness(1) blur(0px)", // Nessuna opacità, nessun filtro
           }}
         />
